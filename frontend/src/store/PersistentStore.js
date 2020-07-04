2ae3e4a5e2bacd51ddd0f7a5df16a00dec0cc8fc
Johann Schopplich <mail@johannschopplich.com>
2020-07-04 17:07:06 +0200

    Fix: Support incognito mode (bail IndexedDB)

--- frontend/src/store/PersistentStore.js
+++ frontend/src/store/PersistentStore.js
@@ -1,12 +1,12 @@
 import { Store } from './Store'
-import { ref, watch } from 'vue'
+import { watch } from 'vue'
 import { set, get } from 'idb-keyval'
 
 export class PersistentStore extends Store {
   constructor (storeName) {
     super(storeName)
     this.storeName = storeName
-    this.isInitialized = ref(false)
+    this.isInitialized = false
 
     // Optionally persist state between browser sessions
     // `VITE_PERSIST_API_STORE` returns a string, but we need a boolean
@@ -18,23 +18,28 @@ export class PersistentStore extends Store {
     if (!this.persistState) return
 
     // Catch multiple initialization calls
-    if (this.isInitialized.value) return
+    if (this.isInitialized) return
 
-    // Check if persisted state exists and if so, use it
-    const stateFromIndexedDB = await get(this.storeName)
-    if (stateFromIndexedDB) {
-      Object.assign(this.state, JSON.parse(stateFromIndexedDB))
+    // Mutation operations on a database aren't allowed in incognito mode
+    try {
+      // Check if persisted state exists and if so, use it
+      const stateFromIndexedDB = await get(this.storeName)
+      if (stateFromIndexedDB) {
+        Object.assign(this.state, JSON.parse(stateFromIndexedDB))
+      }
+
+      // Watch for state changes and immediately save it to `IndexedDB`
+      watch(() => this.state, val => {
+        set(this.storeName, JSON.stringify(val))
+      }, { deep: true })
+    } catch (error) {
+      if (process.env.NODE_ENV === 'development') {
+        console.error(error)
+      }
+
+      this.persistState = false
     }
 
-    // Watch for state changes and immediately save it to `IndexedDB`
-    watch(() => this.state, val => {
-      set(this.storeName, JSON.stringify(val))
-    }, { deep: true })
-
-    this.isInitialized.value = true
-  }
-
-  getIsInitialized () {
-    return this.isInitialized
+    this.isInitialized = true
   }
 }
