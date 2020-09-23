f76091b92094481013055262e75c55cb82aa4238
Johann Schopplich <mail@johannschopplich.com>
2020-09-23 11:48:55 +0200

    style(router): cleanup scroll behavior handler

--- frontend/src/router/scrollBehaviour.js
+++ frontend/src/router/scrollBehaviour.js
@@ -1,18 +1,19 @@
 /**
- * Handles scroll behaviour for Vue Router
+ * Handles the scroll behavior on route navigation
  *
  * @param {object} to Route object of next page
  * @param {object} from Route object of previous page
  * @param {object} savedPosition Used by popstate navigations
  * @returns {(object|boolean)} Scroll position or `false`
  */
-export const scrollBehavior = async function (to, from, savedPosition) {
-  // Can be `auto` or `smooth`, defaults to no scroll behaviour
+export function scrollBehavior (to, from, savedPosition) {
+  // Use predefined scroll behavior if defined, defaults to no scroll behavior
   const behavior = document.documentElement.style.scrollBehavior || 'auto'
 
-  // `savedPosition` is only available for popstate navigations
+  // Returning the `savedPosition` (if available) will result in a native-like
+  // behavior when navigating with back/forward buttons
   if (savedPosition) {
-    return { ...savedPosition, behavior }
+    return savedPosition
   }
 
   // Scroll to anchor by returning the selector
@@ -22,10 +23,10 @@ export const scrollBehavior = async function (to, from, savedPosition) {
 
   // Check if any matched route config has meta that discourages scrolling to top
   if (to.matched.some(m => m.meta.scrollToTop === false)) {
-    // Leave scroll as it is by not returning anything
+    // Leave scroll as it is
     return false
   }
 
   // Always scroll to top
-  return { left: 0, top: 0, behavior }
+  return { left: 0, top: 0 }
 }
