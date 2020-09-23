cf157de77ce140a2558a18aeed2e9dd9d234cb34
Johann Schopplich <mail@johannschopplich.com>
2020-09-23 13:27:48 +0200

    perf: preload module template for initial view
    Improves performance if <link rel=modulepreload href=> is supported
    by the browser.

--- site/snippets/vue-index.php
+++ site/snippets/vue-index.php
@@ -5,11 +5,17 @@ use Kirby\Cms\Url;
 $assetsDir = '/assets/';
 $apiLocation = Url::path(env('KIRBY_API_LOCATION', ''), true);
 
-$pathToAsset = function (string $pattern) use ($assetsDir) {
+$assetPath = function (string $pattern) use ($assetsDir) {
   $filename = glob(kirby()->roots()->index() . $assetsDir . $pattern)[0] ?? null;
   if ($filename === null) throw new Exception('No production assets found. You have to bundle the app first. Run `npm run build`.');
   return $assetsDir . basename($filename);
-}
+};
+
+$modulePreloadLink = function (string $name) use ($assetsDir) {
+  $filename = glob(kirby()->roots()->index() . $assetsDir . ucfirst($name) . '.*.js')[0] ?? null;
+  if ($filename === null) return;
+  return '<link rel="modulepreload" href="' . $assetsDir . basename($filename) . '">';
+};
 
 ?>
 <!DOCTYPE html>
@@ -21,13 +27,14 @@ $pathToAsset = function (string $pattern) use ($assetsDir) {
   <?php snippet('meta', compact('page', 'site')) ?>
 
   <link rel="preload" href="<?= $apiLocation . '/home.json' ?>" as="fetch" crossorigin>
-  <link rel="stylesheet" href="<?= $pathToAsset('style.*.css') ?>">
+  <?= $modulePreloadLink($page->intendedTemplate()->name()) ?>
+  <link rel="stylesheet" href="<?= $assetPath('style.*.css') ?>">
 
 </head>
 <body>
 
   <div id="app"></div>
-  <script type="module" src="<?= $pathToAsset('index.*.js') ?>"></script>
+  <script type="module" src="<?= $assetPath('index.*.js') ?>"></script>
 
 </body>
 </html>
