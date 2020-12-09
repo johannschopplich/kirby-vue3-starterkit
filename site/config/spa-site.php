1fb7ffa7880f34fa7839e745ed8810b32516784c
Johann Schopplich <mail@johannschopplich.com>
2020-12-09 20:39:37 +0100

    fix: provide translated titles inside `site`

--- site/config/spa-site.php
+++ site/config/spa-site.php
@@ -1,11 +1,13 @@
 <?php
 
+$language = kirby()->languageCode();
+
 return [
     // (1)
     // The following data is mandatory for the frontend router to initialize:
     'children' => array_values(site()->children()->published()->map(fn($child) => [
         'uri' => $child->uri(),
-        'title' => $child->content()->title()->value(),
+        'title' => $child->content($language)->title()->value(),
         'isListed' => $child->isListed(),
         'template' => $child->intendedTemplate()->name(),
         'childTemplate' => $child->hasChildren() ? $child->children()->first()->intendedTemplate()->name() : null
