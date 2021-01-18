f01494175d8d45716ae9b8b0bb82476c83eed6ca
Johann Schopplich <mail@johannschopplich.com>
2021-01-18 12:07:01 +0100

    feat: example language switcher component

---
+++ frontend/src/components/examples/LanguageSwitcher.vue
@@ -0,0 +1,25 @@
+<template>
+  <a
+    v-for="lang in site.languages.filter(language => language.code !== currentCode)"
+    :key="lang.code"
+    :href="`/${lang.code}`"
+  >
+    {{ lang.name }}
+  </a>
+</template>
+
+<script>
+import { useSite, useLanguages } from '../hooks'
+
+export default {
+  setup () {
+    const site = useSite()
+    const { languageCode: currentCode } = useLanguages()
+
+    return {
+      site,
+      currentCode
+    }
+  }
+}
+</script>
