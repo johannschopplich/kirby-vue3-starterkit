efa5ff48a44427b00c35bf2fcfdeff8a175dce33
Johann Schopplich <mail@johannschopplich.com>
2021-01-18 13:42:23 +0100

    feat: use current path in language switcher

--- frontend/src/components/examples/LanguageSwitcher.vue
+++ frontend/src/components/examples/LanguageSwitcher.vue
@@ -2,22 +2,25 @@
   <a
     v-for="lang in site.languages.filter(language => language.code !== currentCode)"
     :key="lang.code"
-    :href="`/${lang.code}`"
+    :href="`/${lang.code}/${route.path}`"
   >
     {{ lang.name }}
   </a>
 </template>
 
 <script>
+import { useRoute } from 'vue-router'
 import { useSite, useLanguages } from '../hooks'
 
 export default {
   setup () {
     const site = useSite()
+    const route = useRoute()
     const { languageCode: currentCode } = useLanguages()
 
     return {
       site,
+      route,
       currentCode
     }
   }
