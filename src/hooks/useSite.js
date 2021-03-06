270a2773367128ca1d0307dc1372fe588212cd5c
Johann Schopplich <mail@johannschopplich.com>
2021-03-06 22:08:40 +0100

    refactor: no need for initSite anymore

--- src/hooks/useSite.js
+++ src/hooks/useSite.js
@@ -1,19 +1,15 @@
 import { reactive, readonly } from 'vue'
 
+const data = JSON.parse(
+  document.getElementById('site-data').textContent
+)
+
 /**
  * Reactive object for the global `site` data
  *
  * @constant {object}
  */
-const site = reactive({})
-
-/**
- * Initialize global `site` object and store it
- */
-export const initSite = async () => {
-  const data = JSON.parse(document.getElementById('site-data').textContent)
-  Object.assign(site, data)
-}
+const site = reactive(data)
 
 /**
  * Hook for the global `site` object
