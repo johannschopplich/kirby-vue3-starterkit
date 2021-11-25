8efb719ae4b02828e998ba40ca88068144781d23
Johann Schopplich <mail@johannschopplich.com>
2021-11-25 18:32:36 +0100

    refactor: server.php

--- server.php
+++ server.php
@@ -1,17 +1,14 @@
 <?php
 
-$root = __DIR__ . '/public';
-
 $uri = urldecode(
     parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
 );
 
-// This file allows us to emulate Apache's `mod_rewrite`
-// functionality from the built-in PHP web server
-if ($uri !== '/' && file_exists($root . '/' . $uri)) {
+// Emulate Apache's `mod_rewrite` functionality
+if ($uri !== '/' && file_exists($_SERVER['DOCUMENT_ROOT'] . '/' . $uri)) {
     return false;
 }
 
 $_SERVER['SCRIPT_NAME'] = '/index.php';
 
-require_once $root . '/index.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/' . $_SERVER['SCRIPT_NAME'];
