c7117184cdff969eba2f1c300fb5b873e048f9a4
Luke Archer <info@small-systems.org>
2022-09-25 09:31:48 +0200

    feat: add support for .env mode files

--- vite.config.js
+++ vite.config.js
@@ -1,46 +1,52 @@
-import "dotenv/config";
 import { resolve } from "path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import Vue from "@vitejs/plugin-vue";
 import Components from "unplugin-vue-components/vite";
 
-process.env.VITE_BACKEND_URL = `${process.env.KIRBY_DEV_PROTOCOL}://${process.env.KIRBY_DEV_HOSTNAME}:${process.env.KIRBY_DEV_PORT}`;
-process.env.VITE_BACKEND_API_SLUG = process.env.CONTENT_API_SLUG;
-process.env.VITE_MULTILANG = process.env.KIRBY_MULTILANG;
+export default ({ mode }) => {
+  process.env = {
+    ...process.env,
+    ...loadEnv(mode, process.cwd(), ["VITE_", "KIRBY_", "CONTENT_"]),
+  };
 
-const root = "src";
+  process.env.VITE_BACKEND_URL = `${process.env.KIRBY_DEV_PROTOCOL}://${process.env.KIRBY_DEV_HOSTNAME}:${process.env.KIRBY_DEV_PORT}`;
+  process.env.VITE_BACKEND_API_SLUG = process.env.CONTENT_API_SLUG;
+  process.env.VITE_MULTILANG = process.env.KIRBY_MULTILANG;
 
-export default defineConfig(({ mode }) => ({
-  root,
-  base: mode === "development" ? "/" : "/dist/",
+  const root = "src";
 
-  resolve: {
-    alias: {
-      "~/": `${resolve(__dirname, root)}/`,
+  return defineConfig({
+    root,
+    base: mode === "development" ? "/" : "/dist/",
+
+    resolve: {
+      alias: {
+        "~/": `${resolve(__dirname, root)}/`,
+      },
     },
-  },
 
-  build: {
-    outDir: resolve(__dirname, "public/dist"),
-    emptyOutDir: true,
-    manifest: true,
-    rollupOptions: {
-      input: resolve(root, "main.js"),
+    build: {
+      outDir: resolve(__dirname, "public/dist"),
+      emptyOutDir: true,
+      manifest: true,
+      rollupOptions: {
+        input: resolve(root, "main.js"),
+      },
     },
-  },
 
-  plugins: [
-    Vue(),
+    plugins: [
+      Vue(),
 
-    Components({
-      dirs: ["components"],
-    }),
-  ],
+      Components({
+        dirs: ["components"],
+      }),
+    ],
 
-  server: {
-    cors: true,
-    host: "0.0.0.0",
-    port: process.env.VITE_DEV_PORT,
-    strictPort: true,
-  },
-}));
+    server: {
+      cors: true,
+      host: "0.0.0.0",
+      port: process.env.VITE_DEV_PORT,
+      strictPort: true,
+    },
+  })
+};
